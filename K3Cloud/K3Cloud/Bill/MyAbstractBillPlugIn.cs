934e0d411cf1b78b27f423acacd736d974b3bf20
FLY_U <zhanghao97hao@163.com>
2018-05-28 18:28:10 +0800

    添加类的注释

--- K3Cloud/K3Cloud/Bill/MyAbstractBillPlugIn.cs
+++ K3Cloud/K3Cloud/Bill/MyAbstractBillPlugIn.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Kingdee.BOS.Core.Bill.PlugIn;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace K3Cloud.Bill
 {
-    class MyAbstractBillPlugIn
+    public class MyAbstractBillPlugIn: AbstractBillPlugIn//引入金蝶Kingdee.BOS.Core里的类AbstractBillPlugIn
     {
     }
 }
